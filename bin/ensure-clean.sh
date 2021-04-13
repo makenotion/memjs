622f9da4683bfe8e207ba54349c424adacc99ad2
Jake Teton-Landis <just.1.jake@gmail.com>
2021-04-13 11:54:48 -0700

    set up everything to ensure we check in lib

---
+++ bin/ensure-clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -z "$(git status --porcelain)" ]]; then
+  echo "Git looks clean, you're good to go" > /dev/stderr
+else
+  git status
+  echo "ERROR: Git looks dirty, see above. Check in all changes." > /dev/stderr
+  exit 2
+fi
