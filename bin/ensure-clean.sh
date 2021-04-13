cba95ac0681c1a3dd0a51a17d9d905e3c14ec504
Jake Teton-Landis <just.1.jake@gmail.com>
2021-04-13 12:21:18 -0700

    ensure-clean: print the diff pls

--- bin/ensure-clean.sh
+++ bin/ensure-clean.sh
@@ -4,6 +4,7 @@ if [[ -z "$(git status --porcelain)" ]]; then
   echo "Git looks clean, you're good to go" > /dev/stderr
 else
   git status
+  git diff
   echo "ERROR: Git looks dirty, see above. Check in all changes." > /dev/stderr
   exit 2
 fi
